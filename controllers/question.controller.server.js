8ed9728091b362d8f7cdb8ba16bbb86d9396d95d
unknown <he.sha@husky.neu.edu>
2020-04-19 01:07:03 -0400

    add and refactor controllers to use daos

--- controllers/question.controller.server.js
+++ controllers/question.controller.server.js
@@ -1,8 +1,22 @@
-const questionService = require("../services/question.service.server")
-module.exports = (app) => {
-    app.get("/api/quizzes/:qid/questions", (req, res) => {
-        const quizId = req.params['qid']
-        const questions = questionService.findQuestionsForQuiz(quizId)
-        res.json(questions)
-    })
-}
+// const questionService = require("../services/question.service.server")
+// module.exports = (app) => {
+//     app.get("/api/quizzes/:qid/questions", (req, res) => {
+//         const quizId = req.params['qid']
+//         const questions = questionService.findQuestionsForQuiz(quizId)
+//         res.json(questions)
+//     })
+// }
+
+const questionsService = require('../services/questions.service.server');
+module.exports = function(app) {
+    app.get('/api/quizzes/:qid/questions', (req, res) =>
+        questionsService.findQuestionsForQuiz(req.params['qid'])
+            .then(questions => res.json(questions)));
+    app.get('/api/questions', (req, res) =>
+        questionsService.findAllQuestions()
+            .then(allQuestions => res.json(allQuestions)));
+    app.get('/api/questions/:qid', (req, res) =>
+        questionsService.findQuestionById(req.params['qid'])
+            .then(question => res.json(question)))
+};
+
--- controllers/quiz.controller.server.js
+++ controllers/quiz.controller.server.js
@@ -1,11 +1,22 @@
-const quizService = require('../services/quiz.service.server')
-module.exports = function(app) {
-    app.get('/api/quizzes', (req, res) => {
-        res.send(quizService.findAllQuizzes())
-    })
-    app.get('/api/quizzes/:qid', (req, res) => {
-        const quizId = req.params['qid']
-        const quiz = quizService.findQuizById(quizId)
-        res.json(quiz)
-    })
-}
+// const quizService = require('../services/quiz.service.server')
+// module.exports = function(app) {
+//     app.get('/api/quizzes', (req, res) => {
+//         res.send(quizService.findAllQuizzes())
+//     })
+//     app.get('/api/quizzes/:qid', (req, res) => {
+//         const quizId = req.params['qid']
+//         const quiz = quizService.findQuizById(quizId)
+//         res.json(quiz)
+//     })
+// }
+
+const quizzesService = require('../services/quizzes.services.server');
+module.exports = function (app) {
+    app.get('/api/quizzes', (req, res) =>
+        quizzesService.findAllQuizzes()
+            .then(allQuizzes => res.json(allQuizzes)));
+    app.get('/api/quizzes/:qzid', (req, res) =>
+        quizzesService.findQuizById(req.params['qzid'])
+            .then(quiz => res.json(quiz)))
+};
+
