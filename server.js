8a7ea6c08bb5774693c3f9f2c3678db1151b406b
unknown <he.sha@husky.neu.edu>
2020-04-19 01:09:05 -0400

    [MODIFY] Modify server js and add body parser

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ app.use(function(req, res, next) {
 });
 
 
-require('./controllers/quiz.controller.server')(app);
-require('./controllers/question.controller.server')(app);
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+require('./controllers/quizzes.controller.server')(app);
+require('./controllers/questions.controller.server')(app);
+require('./controllers/quiz-attempts.controller.server')(app);
 app.listen(3000);
 
 console.log('server started!');
