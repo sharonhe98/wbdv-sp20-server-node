b0e175c199f6f8986340f6ba9009d16ffcafb91f
unknown <he.sha@husky.neu.edu>
2020-04-19 01:03:09 -0400

    refactored services

--- services/quiz.service.server.js
+++ services/quiz.service.server.js
@@ -1,8 +1,13 @@
-let quizzes = require('./quizzes')
-const findAllQuizzes = () => quizzes
-const findQuizById = (quizId) =>
-    quizzes.find(quiz => quiz._id === quizId)
-module.exports = {
-    findAllQuizzes,
-    findQuizById
-}
+// let quizzes = require('./quizzes')
+// const findAllQuizzes = () => quizzes
+// const findQuizById = (quizId) =>
+//     quizzes.find(quiz => quiz._id === quizId)
+// module.exports = {
+//     findAllQuizzes,
+//     findQuizById
+// }
+
+const quizzesDao = require('../daos/quizzes.dao.server');
+const findAllQuizzes = () => quizzesDao.findAllQuizzes();
+const findQuizById = (qid) => quizzesDao.findQuizById(qid);
+module.exports = { findAllQuizzes, findQuizById };
